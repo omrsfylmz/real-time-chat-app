359ce46a881270b86f5f3cff3f0c003f89efec38
Ömer Sefa YILMAZ <omrsfylmz@gmail.com>
2023-06-01 22:08:07 +0300

    feat: add form for auth

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import Button from "@/app/components/Button";
+import Input from "@/app/components/inputs/Input";
+import { type } from "os";
+import { useCallback, useState } from "react";
+import { FieldValues, SubmitHandler, set, useForm } from "react-hook-form";
+type Variant = "LOGIN" | "REGISTER";
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>("LOGIN");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === "LOGIN") {
+      setVariant("REGISTER");
+    } else {
+      setVariant("LOGIN");
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      email: "",
+      password: "",
+      name: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setLoading(true);
+
+    if (variant === "REGISTER") {
+      // register
+    }
+
+    if (variant === "LOGIN") {
+      // login
+    }
+
+    const socialAction = (action: string) => {
+      setLoading(true);
+      // social login
+    };
+  };
+
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === "REGISTER" && (
+            <Input id="name" label="Name" register={register} errors={errors} />
+          )}
+          <Input
+            id="email"
+            label="Email adress"
+            type="email"
+            register={register}
+            errors={errors}
+          />
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            register={register}
+            errors={errors}
+          />
+          <div>
+            <Button>txt</Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AuthForm;
