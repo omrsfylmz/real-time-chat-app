2f48a4aadb7b496049b0b4031f21ddb475017dc2
Ömer Sefa YILMAZ <omrsfylmz@gmail.com>
2023-06-03 23:08:12 +0300

    refactor: linter

--- app/users/components/UserList.tsx
+++ app/users/components/UserList.tsx
@@ -12,15 +12,15 @@ const UserList: React.FC<UserListProps> = ({ items }) => {
   return (
     <aside
       className="
-        fixed
-        inset-y-0
+        fixed 
+        inset-y-0 
         pb-20
         lg:pb-0
-        lg:left-20
-        lg:w-80
+        lg:left-20 
+        lg:w-80 
         lg:block
-        overflow-y-auto
-        border-r
+        overflow-y-auto 
+        border-r 
         border-gray-200
         block w-full left-0
       "
@@ -29,9 +29,9 @@ const UserList: React.FC<UserListProps> = ({ items }) => {
         <div className="flex-col">
           <div
             className="
-              text-2xl
-              font-bold
-              text-neutral-800
+              text-2xl 
+              font-bold 
+              text-neutral-800 
               py-4
             "
           >
