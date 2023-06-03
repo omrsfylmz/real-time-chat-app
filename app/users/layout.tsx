b02082c13ec5ffe431f092eebf84dcbc77ae80d6
Ömer Sefa YILMAZ <omrsfylmz@gmail.com>
2023-06-03 23:07:32 +0300

    refactor: delete semicoloumn

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -2,20 +2,20 @@ import getUsers from "../actions/getUsers";
 import Sidebar from "../components/sidebar/Sidebar";
 import UserList from "./components/UserList";
 
-export default async function UserLayout({
+export default async function UsersLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   const users = await getUsers();
+
   return (
-    // @ts-expect-error Server Components
+    // @ts-expect-error Server Component
     <Sidebar>
       <div className="h-full">
         <UserList items={users} />
         {children}
       </div>
-      ;
     </Sidebar>
   );
 }
