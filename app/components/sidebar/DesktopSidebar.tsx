e916f2bd8bcf9eaa5cb6b13e5e0ef1a0031354ae
Ömer Sefa YILMAZ <omrsfylmz@gmail.com>
2023-06-03 22:54:01 +0300

    refactor: fix typo

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -2,7 +2,7 @@
 
 import DesktopItem from "./DesktopItem";
 import useRoutes from "@/app/hooks/useRoutes";
-import SettingsModal from "./SetingsModal";
+import SettingsModal from "./SettingsModal";
 import { useState } from "react";
 import Avatar from "../Avatar";
 import { User } from "@prisma/client";
