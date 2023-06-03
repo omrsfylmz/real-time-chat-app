d400a2ad789bf337b6a609dba2d6f23a95eacebe
Ömer Sefa YILMAZ <omrsfylmz@gmail.com>
2023-06-03 15:14:46 +0300

    fix: update toaster position

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <ToasterContext />
-      <body>{children}</body>
+      <body>
+        <ToasterContext />
+        {children}
+      </body>
     </html>
   );
 }
